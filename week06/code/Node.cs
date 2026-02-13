83a5393c7f99470c72c7c5132aec173f0c50edd0
clebervitorio <clebervitorio@gmail.com>
2026-02-13 22:15:17 +0000

    W06 Code - Trees

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,6 +12,10 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (value == Data)
+        {
+            return;
+        }
 
         if (value < Data)
         {
@@ -20,6 +24,11 @@ public class Node
                 Left = new Node(value);
             else
                 Left.Insert(value);
+        }
+
+        else if (value > Data && Right is not null && Right.Data == value)
+        {
+            return;
         }
         else
         {
@@ -34,12 +43,25 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
+        if (value == Data) {
+            return true;
+        }
+        if (value < Data && Left is not null) {
+            return Left.Contains(value);
+        }
+        if (value > Data && Right is not null) {
+            return Right.Contains(value);
+        }
         return false;
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+
+        return 1 + Math.Max(leftHeight, rightHeight);
+        // return 0; // Replace this line with the correct return statement(s)
     }
 }
